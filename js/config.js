3cea3e59159d5e54b167687a129345d2ee08d47f
retrogamesnetwork <108418893+retrogamesnetwork@users.noreply.github.com>
2022-11-18 15:14:38 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+  apiKey: "AIzaSyCYgieNluU4atVEYhCcUY5GC78eqYXBYsA",
+  authDomain: "AIzaSyCYgieNluU4atVEYhCcUY5GC78eqYXBYsA",
+  databaseURL: "https://retro-games-network-default-rtdb.firebaseio.com",
+  projectId: "retro-games-network",
+  storageBucket: "retro-games-network.appspot.com",
+  messagingSenderId: "1025542945363",
+  appId: "1:1025542945363:web:c4d81a05a8a9d8809564a6",
+  measurementId: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  CometChatAppId: "2258112a8a48b3a8",
+  CometChatRegion: "us",
+  CometChatAuthKey: "668b00da1cedb6b44db126bbd8a0ff26f1911a89",
+  CometChatAPIKey: "2258112a8a48b3a8",
+  CometChatWidgetId: "90882480-7bb2-4815-a48c-7ce929e1ccaa"
+};
